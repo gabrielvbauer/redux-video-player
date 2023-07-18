b0b5b05cd816833cbc17ac4c109a1a6222920515
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2023-07-17 21:37:29 -0300

    User interface

---
+++ src/components/Module.tsx
@@ -0,0 +1,37 @@
+import * as Collapsible from "@radix-ui/react-collapsible"
+import { ChevronDown } from "lucide-react"
+import { Lesson } from "./Lesson"
+import "./Module.css"
+
+interface ModuleProps {
+  moduleIndex: number
+  title: string
+  amountOfLessons: number
+}
+
+export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+  return (
+    <Collapsible.Root className="group">
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+        <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-sm">
+          {moduleIndex + 1}
+        </div>
+
+        <div className="flex flex-col gap-1 text-left">
+          <strong className="text-sm">{title}</strong>
+          <span className="text-sm text-zinc-400">{amountOfLessons} aulas</span>
+        </div>
+
+        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
+
+      <Collapsible.Content className="CollapsibleContent">
+        <nav className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Fundamentos do Redux" duration="09:15" />
+          <Lesson title="Fundamentos do Redux" duration="09:15" />
+          <Lesson title="Fundamentos do Redux" duration="09:15" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
+  )
+}
