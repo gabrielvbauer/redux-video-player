b721b0cbf6d755c51717e1de9c8dd689456a6465
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2023-07-18 22:03:48 -0300

    Redux async thunk

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -2,8 +2,26 @@ import { MessageCircle } from "lucide-react"
 import { Header } from "../components/Header"
 import { Video } from "../components/Video"
 import { Module } from "../components/Module"
+import { useAppDispatch, useAppSelector } from "../store"
+import { loadCourse, useCurrentLesson } from "../store/slices/player"
+import { useEffect } from "react"
 
 export function Player() {
+  const dispatch = useAppDispatch()
+  const modules = useAppSelector((state) => state.player.course?.modules)
+
+  const { currentLesson } = useCurrentLesson()
+
+  useEffect(() => {
+    dispatch(loadCourse())
+  }, [])
+
+  useEffect(() => {
+    if (currentLesson) {
+      document.title = `Assistindo ${currentLesson.title}`
+    }
+  }, [currentLesson])
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
@@ -22,21 +40,15 @@ export function Player() {
           </div>
 
           <aside className="w-80 border-l border-zinc-800 bg-zinc-900 overflow-y-auto divide-y-2 divide-zinc-900 absolute top-0 right-0 bottom-0 scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            <Module
-              moduleIndex={0}
-              title="Desvendando o Redux"
-              amountOfLessons={3}
-            />
-            <Module
-              moduleIndex={1}
-              title="Desvendando o Redux"
-              amountOfLessons={3}
-            />
-            <Module
-              moduleIndex={2}
-              title="Desvendando o Redux"
-              amountOfLessons={3}
-            />
+            {modules &&
+              modules.map((module, index) => (
+                <Module
+                  key={module.id}
+                  moduleIndex={index}
+                  title={module.title}
+                  amountOfLessons={module.lessons.length}
+                />
+              ))}
           </aside>
         </main>
       </div>
